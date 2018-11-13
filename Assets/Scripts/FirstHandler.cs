7c85990f19addef3c34746613f8fe4128fb6729e
azureabyss <joe1996.0810@gmail.com>
2018-11-13 19:09:35 +0800

    add Assets

---
+++ Assets/Scripts/FirstHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;  //DIY
+
+public class FirstHandler : MonoBehaviour {
+	
+	public void Botton(string name)
+	{
+		
+		SceneManager.LoadScene(name, LoadSceneMode.Single);
+	}
+
+
+
+}
+
